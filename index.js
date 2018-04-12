de1cace3293f45c17cd59a987b71f4f46ccdb0b8
Steve <steve@Alisons-MacBook-Air.local>
2018-04-12 11:14:03 -0500

    Set up promise to load app dependencies

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 
+// dynamically load a script
 function loadScript(url) {
   var script = document.createElement("script")
   script.src = url
-
   document.body.appendChild(script)
 }
-loadScript('app/constants.js')
-loadScript('app/models/household-entry.js')
-loadScript('app/services/sanitizer.js')
-loadScript('app/services/dom-registry.js')
-loadScript('app/services/household-store.js')
-loadScript('app/views/household-list.js')
-loadScript('app/views/household-debug.js')
-loadScript('app/controllers/household-controller.js')
 
-// TODO: promise instead
-setTimeout(function() {
-  // start the party
+// load script dependencies
+function loadScripts() {
+  return new Promise(function(resolve, reject) {
+    loadScript('app/constants.js')
+    loadScript('app/models/household-entry.js')
+    loadScript('app/services/sanitizer.js')
+    loadScript('app/services/dom-registry.js')
+    loadScript('app/services/household-store.js')
+    loadScript('app/views/household-list.js')
+    loadScript('app/views/household-debug.js')
+    loadScript('app/controllers/household-controller.js')
+
+    // loop until all dependencies are loaded
+    var timer = setInterval(function() {
+      if (
+        'object' === typeof validRelationships
+        && 'function' === typeof HouseholdEntry
+        && 'function' === typeof sanitizeInput
+        && 'object' === typeof DomRegistry
+        && 'object' === typeof HouseholdStore
+        && 'object' === typeof HouseholdListView
+        && 'object' === typeof HouseholdDebugView
+        && 'object' === typeof HouseholdController
+      ) {
+        clearInterval(timer)
+        resolve(true)
+      }
+    }, 10)
+  })
+}
+
+// initialize app once scripts have loaded
+loadScripts().then(function() {
   HouseholdController.initialize()
-}, 100);
+  console.log('Ready')
+})
